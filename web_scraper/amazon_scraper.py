427d6658393b1ff9ed287edba7e08b80a0a429c9
saif-dev-19 <anonymous.safu@gmail.com>
2026-06-21 11:57:02 +0600

    BS4 added

--- web_scraper/amazon_scraper.py
+++ web_scraper/amazon_scraper.py
@@ -6,3 +6,10 @@ import bs4
 
 if __name__ == "__main__":
     print("Starting...")
+    with open("amazon_products_urls.csv",newline='') as csvfile:
+        reader = csv.reader(csvfile,delimiter=',')
+
+        for row in reader:
+            url = row[0]
+
+            print(url)
